3baf57343bf12d31aac9f24d5b3dd5b2e497ecbc
guilhermevargasnegociosonline-pixel <guilhermevargasnegociosonline@gmail.com>
2026-06-19 17:30:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'domnavalha-v1';
+const ASSETS = ['/', '/index.html', '/admin.html', '/logo.jpg', '/johnny.jpg', '/corte1.jpg', '/corte2.jpg', '/corte3.jpg', '/corte4.jpg', '/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
