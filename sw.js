b5b38f2026052d0af7aec1659921c095a2de2f00
guilhermevargasnegociosonline-pixel <guilhermevargasnegociosonline@gmail.com>
2026-06-19 21:18:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
-const CACHE = 'domnavalha-v1';
-const ASSETS = ['/', '/index.html', '/admin.html', '/logo.jpg', '/johnny.jpg', '/corte1.jpg', '/corte2.jpg', '/corte3.jpg', '/corte4.jpg', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'domnavalha-v10';
+const ASSETS = ['/index.html', '/admin.html', '/logo.jpg', '/johnny.jpg', '/corte1.jpg', '/corte2.jpg', '/corte3.jpg', '/corte4.jpg', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
